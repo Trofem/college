4ad4ebf3ed2041188003c6ddd430e740f201ebc3
trofim.al <127919576+Trofem@users.noreply.github.com>
2025-05-27 22:31:22 +0300

    очень важное изменение

--- telebot/calculator.py
+++ telebot/calculator.py
@@ -1,16 +1,45 @@
-log = []
-while True:
-    num1, num2, sym = None, None, None
-    try:
-        num1, num2, sym = float(input("Введите первое число: ")), float(input("Введите второе число: ")), input("Введите символ операции: ")
-    except:
-        print("Числа были неправильно введены.")
-        continue
 
-    if ("exit" in sym): 
-        break
-    result = eval(f"{num1}{sym}{num2}") 
-    log.append(f"Вычисление: {num1} {sym} {num2} = {result}")
-    print(F'Результат: {result}')
-print("Конец")
-print('\n'.join(log))
+class Calculator:
+    def __init__(self):
+        self.log = []
+        while True:
+            
+            res = self.main_loop()
+            if res == 1:
+                break
+
+        print("Конец")
+        print('\n'.join(self.log))
+
+    def _is_exit(self, text):
+        return text.strip().lower() == "exit"
+                
+    
+    def main_loop(self):
+        try:
+            num1, num2, sym = None, None, None
+
+            num1 = (input("Введите первое число: ")).strip()
+            if self._is_exit(num1):
+                return 1
+                    
+            num2 = (input("Введите второе число: ")).strip()
+            if self._is_exit(num2):
+                return 1
+
+            sym = input("Введите символ операции: ").strip()
+            if self._is_exit(sym):
+                return 1
+            elif (sym == '/' and num2 == '0'):
+                print("Деление на ноль недопустимо!")
+                return
+            num1, num2 = float(num1), float(num2)
+        except:
+            print("Числа были неправильно введены.")
+            return #не заканчивает
+
+        result = eval(f"{num1}{sym}{num2}")
+        self.log.append(f"Вычисление: {num1} {sym} {num2} = {result}")
+        print(F'Результат: {result}')
+
+Calculator()
